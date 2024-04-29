3a83945b188d8c4fc0c767103191d80504783f85
rajat <mahajanrohit0042@gmail.com>
2024-04-29 20:05:28 +0530

    FirstCommit

---
+++ frontend/.vite/deps_temp_6718d59e/react-redux.js
@@ -0,0 +1,32 @@
+import {
+  Provider_default,
+  ReactReduxContext,
+  connect_default,
+  createDispatchHook,
+  createSelectorHook,
+  createStoreHook,
+  import_react_dom,
+  shallowEqual,
+  useDispatch,
+  useSelector,
+  useStore
+} from "./chunk-GG3YD35E.js";
+import "./chunk-HNHD5ZHX.js";
+import "./chunk-IQVVG4VM.js";
+import "./chunk-UOIPVTYB.js";
+import "./chunk-AUZ3RYOM.js";
+var export_batch = import_react_dom.unstable_batchedUpdates;
+export {
+  Provider_default as Provider,
+  ReactReduxContext,
+  export_batch as batch,
+  connect_default as connect,
+  createDispatchHook,
+  createSelectorHook,
+  createStoreHook,
+  shallowEqual,
+  useDispatch,
+  useSelector,
+  useStore
+};
+//# sourceMappingURL=react-redux.js.map
